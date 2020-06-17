95068a0c38b83c9effd644ca65ba27f0c3012c10
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-06-17 16:53:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ main.remove()
 
 const newHeader = document.createElement('h1')
 newHeader.id = 'victory';
-newHeader.innerHtml = 'Jrodr4544 is the champion';
+newHeader.innerHTML = 'Jrodr4544 is the champion';
 // document.body.appendChild()
 // getElementById();
