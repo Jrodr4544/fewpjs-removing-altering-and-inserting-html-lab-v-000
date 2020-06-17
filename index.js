b24e5c9a740462f6f7b25a8b13afcfd9f6b6b4a4
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-06-17 16:50:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 let main = document.getElementById('main');
 main.remove()
 
-let newHeader = document.createElement('h1#victory')
+const newHeader = document.createElement('h1#victory')
 // document.body.appendChild()
 // getElementById();
